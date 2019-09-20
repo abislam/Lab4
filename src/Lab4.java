dbd5608dc133b57a5779490b5e8607067d2cc861
Abed Islam <aislam360@gmail.com>
2019-09-19 22:43:01 -0400

    Updated comments for the checklist in the beginning of the program

--- src/Lab4.java
+++ src/Lab4.java
@@ -3,33 +3,37 @@ import java.util.Random;
 
 public class Lab4 {
 	public static void main(String args[]){
-		//First ask the user for a level and a problem type.
+		//First ask the user for a level and a problem type. (done)
 		
-		//You need to validate the level and problem type and loop until the user enters a correct one.
+		//You need to validate the level and problem type and loop until the user enters a correct one. (done)
 		
 		/*There should be 3 levels. Level 1 operands would have values in the range of 0-9, 
 		 * level 2 operands would have values in the range of 0-99, 
-		 * and level 3 operands would have values in the range of 0-999.*/
+		 * and level 3 operands would have values in the range of 0-999.
+		 * (done)*/
 		
 		
-		//Each problem will consist of three randomly generated operands.
+		//Each problem will consist of three randomly generated operands. (done)
 		
 		/* There should be 4 problem types. Problem type 1 requires the student to find the sum of the three numbers, 
 		 * problem type 2 requires the user to find the integer average of the three numbers, 
 		 * problem type 3 requires the user to find the largest of the three numbers, 
-		 * and problem type 4 requires the user to find the smallest of the three numbers.*/
+		 * and problem type 4 requires the user to find the smallest of the three numbers. 
+		 * (done)*/
 		
-		//The program should ask the user 10 questions.
+		//The program should ask the user 10 questions. (done)
 		
 		/*The program should randomly generate the numbers for each problem and display them to the user. 
-		 * Then the program should get the users answer and check that answer.*/
+		 * Then the program should get the users answer and check that answer.
+		 * (done)*/
 		
 		/*The program should provide individual feedback for each problem. 
 		 * There should be 3 different positive and 3 different negative feedbacks chosen from for each problem.*/
 		
 		/*After the user finishes their 10 problems, display the number they got right 
 		 * and then query them if they want to play again. 
-		 * If they choose to play again, get a new level and problem type before asking 10 new problems. */
+		 * If they choose to play again, get a new level and problem type before asking 10 new problems. 
+		 * (done)*/
 		
 		//generate the random numbers based on level selected
 		Random rand = new Random();
